c81007fc2a6d0d522624f15fbcbb9a73b5407cca
Jared DiScipio <jdiscipi@uvm.edu>
2021-12-09 19:35:26 -0500

    document

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
 include 'top.php';
+
+// Destroying the session will remove the username, deauthenticating the user
+// User authentication is predicated on the use of the username session variable
 session_destroy();
 header("Location: index.php");
 ?>
